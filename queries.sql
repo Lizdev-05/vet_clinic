07446fdf552060b80eee64e2f71852a518476ab6
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-14 13:48:57 +0100

    Complete queries

--- queries.sql
+++ queries.sql
@@ -47,3 +47,18 @@ ALTER TABLE animals  DROP COLUMN specie;
 -- Modify inserted animal to have specie-id
 UPDATE animals SET species_id = '2' WHERE name LIKE '%mon';
 UPDATE animals SET species_id = '1' WHERE name NOT LIKE '%mon';
+
+SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.id WHERE owners.full_name = 'Melody Pond';
+SELECT * FROM animals JOIN species ON animals.species_id = species.id WHERE species.name = 'Pokemon';
+
+SELECT animals.name, owners.full_name FROM animals RIGHT JOIN owners ON animals.owners_id = owners.id;
+
+SELECT species.name, COUNT(animals.species_id) FROM animals JOIN species ON species.id = animals.species_id GROUP BY species.name;
+
+SELECT animals.name from animals JOIN owners ON owners.id = animals.owners_id JOIN species on species.id = animals.species_id WHERE animals.species_id = '2' AND animals.owners_id = '2';
+
+SELECT animals.name from animals JOIN owners ON owners.id = animals.owners_id WHERE animals.escape_attempt = '0' AND animals.owners_id = '5';
+
+SELECT owners.full_name FROM owners JOIN animals ON animals.owners_id COUNT(animals.id) = (SELECT MAX(animals.id) FROM animals;
+
+SELECT full_name, COUNT(owners_id) FROM owners JOIN animals on owners.id = animals.owners_id GROUP BY full_name ORDER BY COUNT (owners_id) desc limit 1;
