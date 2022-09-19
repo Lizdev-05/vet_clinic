a993b7f47a2c3fb74a6ac15eec1dfaac3e8c4c01
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-19 13:32:07 +0100

    complete query

--- queries.sql
+++ queries.sql
@@ -92,6 +92,6 @@ SELECT species.name, COUNT(animals.species_id) FROM animals JOIN visits ON anima
 -- Performance
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
 
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
 
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
