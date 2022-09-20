99eab47896ca280885440a025cfa78eeeef73c57
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-20 12:23:25 +0100

    add table for the medical histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,12 @@ CREATE TABLE patients (
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL,
 );
+
+CREATE TABLE medical_histories (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    admitted_at timestamp
+    patient_id BIGINT REFERENCES patients (id),
+);
+
+
+
