2ffe962a9932869a905bcf6b908f9ab2b735ae28
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-20 12:33:53 +0100

    add table for the invoice

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,14 +3,24 @@ CREATE DATABASE clinic;
 CREATE TABLE patients (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
+    date_of_birth DATE NOT NULL
 );
 
 CREATE TABLE medical_histories (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    admitted_at timestamp
-    patient_id BIGINT REFERENCES patients (id),
+    admitted_at timestamp,
+    patient_id BIGINT REFERENCES patients (id)
 );
+
+
+CREATE TABLE invoices (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_histories_id BIGINT REFERENCES medical_histories (id)
+);
+
 
 
 
